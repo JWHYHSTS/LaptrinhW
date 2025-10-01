589c048317d848ed5201e56b9801f52f7a6670bf
JWHYHSTS <haidangblck@gmail.com>
2025-10-01 13:51:42 +0700

    game_caro_gk

---
+++ Caro/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace BTL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRedo = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnCom = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlBanco = new System.Windows.Forms.Panel();
+            this.btnPvP = new System.Windows.Forms.Button();
+            this.btnAlphaBeta = new System.Windows.Forms.Button();
+            this.timerTurn = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnreset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRedo
+            // 
+            this.btnRedo.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRedo.Location = new System.Drawing.Point(48, 764);
+            this.btnRedo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnRedo.Name = "btnRedo";
+            this.btnRedo.Size = new System.Drawing.Size(298, 80);
+            this.btnRedo.TabIndex = 4;
+            this.btnRedo.Text = "Redo";
+            this.btnRedo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRedo.UseVisualStyleBackColor = true;
+            this.btnRedo.Click += new System.EventHandler(this.btnRedo_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(48, 973);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(298, 80);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUndo.Location = new System.Drawing.Point(48, 664);
+            this.btnUndo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(298, 80);
+            this.btnUndo.TabIndex = 2;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnCom
+            // 
+            this.btnCom.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCom.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCom.Location = new System.Drawing.Point(48, 462);
+            this.btnCom.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCom.Name = "btnCom";
+            this.btnCom.Size = new System.Drawing.Size(298, 80);
+            this.btnCom.TabIndex = 1;
+            this.btnCom.Text = "Easy Mode";
+            this.btnCom.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCom.UseVisualStyleBackColor = true;
+            this.btnCom.Click += new System.EventHandler(this.btnCom_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BTL.Properties.Resources.Caro;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 442);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlBanco
+            // 
+            this.pnlBanco.BackColor = System.Drawing.Color.Lime;
+            this.pnlBanco.Location = new System.Drawing.Point(477, 103);
+            this.pnlBanco.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pnlBanco.Name = "pnlBanco";
+            this.pnlBanco.Size = new System.Drawing.Size(1004, 966);
+            this.pnlBanco.TabIndex = 5;
+            this.pnlBanco.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBanco_Paint);
+            this.pnlBanco.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlBanco_MouseClick);
+            // 
+            // btnPvP
+            // 
+            this.btnPvP.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPvP.Location = new System.Drawing.Point(48, 869);
+            this.btnPvP.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnPvP.Name = "btnPvP";
+            this.btnPvP.Size = new System.Drawing.Size(298, 80);
+            this.btnPvP.TabIndex = 6;
+            this.btnPvP.Text = "P vs P";
+            this.btnPvP.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPvP.UseVisualStyleBackColor = true;
+            this.btnPvP.Click += new System.EventHandler(this.btnPvP_Click);
+            // 
+            // btnAlphaBeta
+            // 
+            this.btnAlphaBeta.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlphaBeta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAlphaBeta.Location = new System.Drawing.Point(48, 562);
+            this.btnAlphaBeta.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAlphaBeta.Name = "btnAlphaBeta";
+            this.btnAlphaBeta.Size = new System.Drawing.Size(298, 80);
+            this.btnAlphaBeta.TabIndex = 7;
+            this.btnAlphaBeta.Text = "Hard Mode";
+            this.btnAlphaBeta.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAlphaBeta.UseVisualStyleBackColor = true;
+            this.btnAlphaBeta.Click += new System.EventHandler(this.btnAlphaBeta_Click);
+            // 
+            // timerTurn
+            // 
+            this.timerTurn.Interval = 1000;
+            this.timerTurn.Tick += new System.EventHandler(this.timerTurn_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Arial", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(1378, 11);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(110, 79);
+            this.lblTime.TabIndex = 8;
+            this.lblTime.Text = "00";
+            // 
+            // btnreset
+            // 
+            this.btnreset.BackColor = System.Drawing.Color.White;
+            this.btnreset.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreset.ForeColor = System.Drawing.Color.Black;
+            this.btnreset.Location = new System.Drawing.Point(372, 674);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(87, 60);
+            this.btnreset.TabIndex = 9;
+            this.btnreset.Text = "Reset";
+            this.btnreset.UseVisualStyleBackColor = false;
+            this.btnreset.Click += new System.EventHandler(this.btnreset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1503, 1097);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnAlphaBeta);
+            this.Controls.Add(this.btnPvP);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnRedo);
+            this.Controls.Add(this.pnlBanco);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnCom);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cờ caro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCom;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnRedo;
+        private System.Windows.Forms.Panel pnlBanco;
+        private System.Windows.Forms.Button btnPvP;
+        private System.Windows.Forms.Button btnAlphaBeta;
+        private System.Windows.Forms.Timer timerTurn;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnreset;
+    }
+}
+
